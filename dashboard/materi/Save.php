f7f4bd8d091c5729263fe60357ed6791a1adf2a8
randi <randi@inforsys.co.id>
2016-11-01 16:04:10 +0700

    menyelesaikan save upload

--- dashboard/materi/Save.php
+++ dashboard/materi/Save.php
@@ -18,44 +18,40 @@ if (isset($_POST["Save"])) {
     $KelasBimbel_Name = $_REQUEST["KelasBimble_Name"];
     $Kelas_Name = $_REQUEST["Kelas_Name"];
     $directory = "../../Upload/$KelasBimbel_Name";
-    
+    $DirectoryFull="";
+    $SubFolder="";
+    $Date = date('Y-m-d H:i:s');
     if (is_dir($directory)==TRUE) {
-        $SubFolder = $directory.'/'.$Kelas_Name.'/';
-        
+        $SubFolder = $directory.'/'.$Kelas_Name;
         if (is_dir($SubFolder)==TRUE) {
-            chmod($SubFolder, 755);   // decimal; probably incorrect
-            chmod($SubFolder, 0755);  // octal; correct value of mode
-            if (move_uploaded_file($_FILES["data_upload"]["tmp_name"], $SubFolder)) {
-                echo'Berhasil Di simpan file dengan nama'."$file_name";
-            }
-            else{
-                echo 'Gagal Disimpan file dengan nama '."$file_name";
-            }
+            $DirectoryFull = $SubFolder.'/'.$file_name;
         } else {
             mkdir($SubFolder);
-            chmod($SubFolder, 755);   // decimal; probably incorrect
-            chmod($SubFolder, 0755);  // octal; correct value of mode
-            if (move_uploaded_file($_FILES["data_upload"]["tmp_name"], $SubFolder)) {
-                echo'Berhasil Di simpan file dengan nama'."$file_name";
-            }
-            else{
-                echo 'Gagal Disimpan file dengan nama '."$file_name";
-            }
+            $DirectoryFull = $SubFolder.'/'.$file_name;
         }
     }
     else{
         mkdir($directory);
-        $SubFolder = $directory.'/'.$Kelas_Name.'/';
+        $SubFolder = $directory.'/'.$Kelas_Name;
         mkdir($SubFolder);
-        chmod($SubFolder, 755);   // decimal; probably incorrect
-        chmod($SubFolder, 0755);  // octal; correct value of mode
-        if (move_uploaded_file($_FILES["data_upload"]["tmp_name"], $SubFolder)) {
-            echo'Berhasil Di simpan file dengan nama'."$file_name";
+        $DirectoryFull = $SubFolder.'/'.$file_name;
+    }
+    $Sql = "INSERT INTO materi(bimbel_id, kelas_id, judul, keterangan, date, file_location)".
+           "VALUES ('$KelasBimbel','$Kelas','$Judul','$Keterangan','$Date','$DirectoryFull')";
+    
+    if ($koneksi->query($Sql)===TRUE) {
+        if (move_uploaded_file($_FILES["data_upload"]["tmp_name"], $SubFolder.'/'.$file_name)) {
+            echo 'Data Tersimpan';
         }
         else{
             echo 'Gagal Disimpan file dengan nama '."$file_name";
         }
     }
+    else{
+        echo "Error: " . $sql . "<br>" . $koneksi->error;
+    }
+    $koneksi->close();
+    
 }
 /*$sql = "INSERT INTO materi(bimbel_id, kelas_id, judul, keterangan, date, file_location) VALUES ('$KelasBimbel','$Kelas','$Judul','$Keterangan','$tanggal','$File')";
 $tambahdata = mysqli_query($sql, $koneksi) or die('Could not look up user information; ' . mysqli_error($koneksi));;
